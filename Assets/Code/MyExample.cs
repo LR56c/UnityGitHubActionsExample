77debe12fcc0bd787be0edcf3d6e55d2c6a5b6fe
LR56c <ronalli56@gmail.com>
2021-01-16 22:40:01 -0300

    test

---
+++ Assets/Code/MyExample.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MyExample : MonoBehaviour
+    {
+        private List<int> numbers = new List<int>();
+
+        private void Start()
+        {
+            
+        }
+    }
+}
